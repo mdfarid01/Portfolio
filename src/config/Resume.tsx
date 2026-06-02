88531de6c9d66d1454e9bc7a0dc4e620876eb61c
Md Farid <mdfarid.0118@gmail.com>
2026-06-03 01:47:23 +0530

    Update resume link

--- src/config/Resume.tsx
+++ src/config/Resume.tsx
@@ -1,3 +1,3 @@
 export const resumeConfig = {
-  url: 'https://drive.google.com/file/d/138rT2SyoqHxDg584hxGEGOeJSrsqQJbp/view?usp=share_link',
+  url: 'https://drive.google.com/file/d/1aArZN1K3KvysuUE8S9S3DimnMSTQ6GGw/view?usp=sharing',
 };
